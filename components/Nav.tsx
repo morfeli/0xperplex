eccd7209acfeae3281cf7c60435bd7bd2b794839
morfeli <fmoreira95@hotmail.com>
2025-01-05 18:26:48 -0500

    feat: active nav link style

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,9 +1,19 @@
+"use client";
 import Image from "next/image";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 import OxPerplexLogo from "../public/assets/0xperplex-logo.png";
 
 export function Nav() {
+  const pathname = usePathname();
+
+  const navItems = [
+    { name: "Home", path: "/" },
+    { name: "About", path: "/about" },
+    { name: "Contact", path: "/contact" },
+  ];
+
   return (
     <header className="">
       <div className="mx-auto py-2 flex justify-between items-center bg-black w-full px-2 xl:px-10">
@@ -19,13 +29,17 @@ export function Nav() {
         </Link>
         <nav className="flex items-center">
           <ul className="flex space-x-4 text-white font-mono text-sm">
-            {["Home", "About", "Contact"].map((item) => (
-              <li key={item}>
+            {navItems.map((item) => (
+              <li key={item.name}>
                 <Link
-                  href={item === "Home" ? "/" : `/${item.toLowerCase()}`}
-                  className="hover:text-purple-300 transition-colors duration-200 ease-in-out"
+                  href={item.path}
+                  className={`transition-colors duration-200 ease-in-out ${
+                    pathname === item.path
+                      ? "text-purple-300"
+                      : "hover:text-purple-200"
+                  }`}
                 >
-                  {item}
+                  {item.name}
                 </Link>
               </li>
             ))}
